48894e5595e07dd1d96502edab7cc6bd9d30cd87
pankaj023 <pankajmehta023@gmail.com>
2019-11-03 17:36:24 +0530

    okay

--- src/main/java/io/valley/school/ValleySchoolsApplication.java
+++ src/main/java/io/valley/school/ValleySchoolsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ValleySchoolsUserDetailsApplication {
+public class ValleySchoolsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ValleySchoolsUserDetailsApplication.class, args);
+		SpringApplication.run(ValleySchoolsApplication.class, args);
 		System.out.println("hello");
 	}
 
